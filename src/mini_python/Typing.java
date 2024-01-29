fec14b5548fcacbeb6d01af16bb81a81cfeb25e7
NathanaeldeLaChesnais <nathanael.houitte-de-la-chesnais@polytechnique.edu>
2024-01-29 18:35:08 +0100

    Début du typage pendant la première scéance de TP.

--- src/mini_python/Typing.java
+++ src/mini_python/Typing.java
@@ -10,8 +10,59 @@ class Typing {
   }
 
   static TFile file(File f) {
-    error(null, "TODO");
-    return null;
+    TFile tf = new TFile();
+    System.out.println("test");
+    System.out.println(f.l.getLast().toString());
+    System.out.println("t=======");
+
+    // 2. The names of the functions declared with def are pairwise distinct, and distinct from len, list, and range.
+    for (Def s : f.l) {
+      String name = s.f.id;
+      if (name.equals("len") || name.equals("list") || name.equals("range")) {
+        error(null, "The names of the functions declared with def should be distinct from len, list, and range.");
+      }
+      for (TDef s2 : tf.l) {
+        if (s2.f.name.equals(name)) {
+          error(null, "The names of the functions declared with def should be distinct from each other.");
+        }
+      }
+      // 5. The formal parameters of a function must be pairwise distinct.
+      // à faire
+      
+      tf.l.add(new TDef( new Function(name, s.f.params),s.f.s));
+    }
+
+    
+
+
+    // Desormais on explore les statements
+    testStatement(f.s);
+
+    return tf;
+  }
+
+
+  static boolean testStatement(Stmt s){
+    if (s instanceof Sif){
+      boolean t1 = testStatement(((Sif) s).s1);
+      boolean t2 = testStatement(((Sif) s).s2);
+      return t1 && t2;
+    }
+    if (s instanceof Sreturn || s instanceof Sassign || s instanceof Sprint || s instanceof Sset || s instanceof Seval){
+      return true;
+    }
+    if (s instanceof Sblock){
+      for (Stmt s2 : ((Sblock) s).l){
+        if (!testStatement(s2)){
+          return false;
+        }
+      }
+    }
+    if (s instanceof Sfor){
+      return testStatement(((Sfor) s).s);
+    }
+
+    return true;
   }
 
 }
