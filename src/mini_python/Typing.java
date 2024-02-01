b0c9eb09afe3a1c31a0b3accbf24cb73c1bbdc43
pe712 <pe.baviere@gmail.com>
2024-02-01 10:05:17 +0100

    visitor implem

--- src/mini_python/Typing.java
+++ src/mini_python/Typing.java
@@ -1,7 +1,10 @@
 package mini_python;
 
-class Typing {
+import java.util.HashSet;
+import java.util.LinkedList;
 
+class Typing {
+  private static TyperVisitor typerVisitor = new TyperVisitor();
   static boolean debug = false;
 
   // use this method to signal typing errors
@@ -10,59 +13,193 @@ class Typing {
   }
 
   static TFile file(File f) {
-    TFile tf = new TFile();
-    System.out.println("test");
-    System.out.println(f.l.getLast().toString());
-    System.out.println("t=======");
+    TFile tfile = new TFile();
+    HashSet<String> definedFunctions = new HashSet<String>();
 
-    // 2. The names of the functions declared with def are pairwise distinct, and distinct from len, list, and range.
-    for (Def s : f.l) {
-      String name = s.f.id;
+    for (Def def : f.l) {
+      String name = def.f.id;
       if (name.equals("len") || name.equals("list") || name.equals("range")) {
-        error(null, "The names of the functions declared with def should be distinct from len, list, and range.");
+        error(def.f.loc, "The names of the functions declared with def should be distinct from len, list, and range.");
       }
-      for (TDef s2 : tf.l) {
-        if (s2.f.name.equals(name)) {
-          error(null, "The names of the functions declared with def should be distinct from each other.");
-        }
+
+      if (definedFunctions.contains(name))
+        error(def.f.loc, "The names of the functions declared with def should be distinct from each other.");
+      else
+        definedFunctions.add(name);
+
+      HashSet<String> definedParameters = new HashSet<String>();
+      LinkedList<Variable> params = new LinkedList<Variable>();
+      for (Ident param : def.l) {
+        String varName = param.id;
+        params.add(Variable.mkVariable(varName));
+        if (definedParameters.contains(varName))
+          error(def.f.loc, "Formal parameters should be pairwise distincts");
+        else
+          definedParameters.add(varName);
       }
-      // 5. The formal parameters of a function must be pairwise distinct.
-      // à faire
-      
-      tf.l.add(new TDef( new Function(name, s.f.params),s.f.s));
+      // TODO check variable scope
+
+      def.s.accept(typerVisitor);
+      TStmt tStmt = typerVisitor.tStmt;
+      tfile.l.add(new TDef(new Function(name, params), tStmt));
     }
-
-    
-
 
     // Desormais on explore les statements
     testStatement(f.s);
 
-    return tf;
+    return tfile;
   }
 
-
-  static boolean testStatement(Stmt s){
-    if (s instanceof Sif){
+  // @Nath TODO je comprend pas trop l'intérêt de ça...
+  static boolean testStatement(Stmt s) {
+    if (s instanceof Sif) {
       boolean t1 = testStatement(((Sif) s).s1);
       boolean t2 = testStatement(((Sif) s).s2);
       return t1 && t2;
     }
-    if (s instanceof Sreturn || s instanceof Sassign || s instanceof Sprint || s instanceof Sset || s instanceof Seval){
+    if (s instanceof Sreturn || s instanceof Sassign || s instanceof Sprint || s instanceof Sset
+        || s instanceof Seval) {
       return true;
     }
-    if (s instanceof Sblock){
-      for (Stmt s2 : ((Sblock) s).l){
-        if (!testStatement(s2)){
+    if (s instanceof Sblock) {
+      for (Stmt s2 : ((Sblock) s).l) {
+        if (!testStatement(s2)) {
           return false;
         }
       }
     }
-    if (s instanceof Sfor){
+    if (s instanceof Sfor) {
       return testStatement(((Sfor) s).s);
     }
 
     return true;
+  }
+
+}
+
+class TyperVisitor implements Visitor {
+  public TStmt tStmt;
+  public TExpr tExpr;
+
+  @Override
+  public void visit(Cnone c) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Cbool c) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Cstring c) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Cint c) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Ecst e) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Ebinop e) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Eunop e) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Eident e) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Ecall e) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Eget e) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Elist e) {
+    LinkedList<TExpr> elmts = new LinkedList<TExpr>();
+    for (Expr exp : e.l) {
+      exp.accept(this);
+      elmts.add(this.tExpr);
+    }
+    this.tExpr = new TElist(elmts);
+  }
+
+  @Override
+  public void visit(Sif s) {
+    s.e.accept(this);
+    s.s1.accept(this);
+    TStmt tStmtSave = this.tStmt;
+    s.s2.accept(this);
+    this.tStmt = new TSif(this.tExpr, tStmtSave, this.tStmt);
+  }
+
+  @Override
+  public void visit(Sreturn s) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Sassign s) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Sprint s) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Sblock s) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Sfor s) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Seval s) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(Sset s) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
   }
 
 }
