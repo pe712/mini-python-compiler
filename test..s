4173dc96beaaacc019ebfe5aa419c69ac05f3cbe
NathanaeldeLaChesnais <nathanael.houitte-de-la-chesnais@polytechnique.edu>
2024-02-12 18:22:01 +0100

    print

---
+++ test..s
@@ -0,0 +1,29 @@
+	.text
+	movq $104, %rdi
+	call my_malloc
+	movq $3, (%rax)
+	movq $11, 8(%rax)
+	movq $104, 8(%rax)
+	movq $101, 16(%rax)
+	movq $108, 24(%rax)
+	movq $108, 32(%rax)
+	movq $111, 40(%rax)
+	movq $32, 48(%rax)
+	movq $119, 56(%rax)
+	movq $111, 64(%rax)
+	movq $114, 72(%rax)
+	movq $108, 80(%rax)
+	movq $100, 88(%rax)
+	leaq %rax, %rdi
+	movq $%s, %rax
+	movq $0, %rax
+	call printf
+	pushq %rbp
+	movq %rsp, %rbp
+	andq $-16, %rsp
+	movq %rbp, rsp
+	popq %rsp
+	ret
+	.data
+__main__:
+my_malloc:
